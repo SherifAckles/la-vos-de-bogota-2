f2e49dd7a51b51cac36fbf2123b6c3217d249dbf
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-03-10 18:24:59 -0500

    modify podcast section

--- src/components/podcast-topics/PodcastTopics.jsx
+++ src/components/podcast-topics/PodcastTopics.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import TopicsHero from '../../images/topics-hero-photo.svg';
+import TopicsHero from '../../images/topics-hero-img.png';
 import './podcast-topics.css';
 
 function PodcastTopics({ podcastCards }) {
   return (
-    <section className="podcast-topics-container">
+    <section className="podcast-topics-container text-dark text-center ">
+      
       <Container>
         <Row>
-          <Col xs={12} md={6}>
-            <div className="card-section">
-              <Row xs={2} md={2} className="g-4">
-                {podcastCards.map((card) => (
-                  <Col key={card.id}>
-                    <div className="card">
-                      <h2>{card.title}</h2>
-                      <p>{card.description}</p>
-                    </div>
-                  </Col>
-                ))}
-              </Row>
-            </div>
-          </Col>
-          <Col xs={12} md={5} className="offset-md-1 ">
+          <Col xs={12} md={6} sm={12} className="hero-container-img">
             <main>
               <picture>
                 <img
                   src={TopicsHero}
                   alt="hero-photo"
-                  className="hero-container-img"
+
                   fluid="true"
                 />
               </picture>
             </main>
           </Col>
+          <Col xs={12} md={6} className="card-section">
+
+            <Row xs={2} md={2} className="g-4">
+              {podcastCards.map((card) => (
+                <Col key={card.id}>
+                  <div className="card">
+                    <h2>{card.title}</h2>
+                    <p>{card.description}</p>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+
+          </Col>
         </Row>
       </Container>
+
     </section>
   );
 }
